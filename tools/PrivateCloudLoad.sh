e660d2febd6b07aa0ff66cf7cd37f120ff56af15
Fabio Giannetti <fgiannet@cisco.com>
2015-10-06 11:18:33 -0700

    Correct the Public Cloud Simulation load
    The Public cloud load has a 20K iteration for each tenant and metric.
    This is wrong and it will generate 30M messages.
    The correct value is 5K and it will generate the expected 7.5M.
    
    Also added documentation reflecting the changes in the code to
    suppor tenant_id, resource_id and date.
    
    Added documentation on how to set the system to deal with high load.
    
    Change-Id: I9ca24cb46828b2a225bb61f720e54b55edb65571

--- tools/PrivateCloudLoad.sh
+++ tools/PrivateCloudLoad.sh
@@ -18,7 +18,7 @@ do
                #echo $tenant_id
                resource_id="${s}_resource_t_${t}"
                #echo $resource_id
-               python ceilosca-message-simulator.py --url rabbit://stackrabbit:password@localhost/ notify-client -m 20000 -s $s -a create -x $tenant_id -r $resource_id -d $date
+               python ceilosca-message-simulator.py --url rabbit://stackrabbit:password@localhost/ notify-client -m 5000 -s $s -a create -x $tenant_id -r $resource_id -d $date
           done
    done
 done
