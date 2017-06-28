2a11d2b5e2ecd01ddd739e4f7ea54becdc929df6
Ashwin Agate <ashwin.agate@suse.com>
2017-06-28 15:01:16 -0700

    Fix ceilosca.sh vagrant env setup
    monasca-ceilometer repo now gets
    copied into the vagrant VM, and
    local.conf gets updated with
    location and branch information,
    so that any change can be
    tested.
    
    Also made following two changes:
    
    1.)
    Fix auth_url property being set in the
    service_credentials section.
    
    2.)
    Create pipeline yaml with meters which
    match monasca_definitions yaml.
    
    Change-Id: I24da3b0df73079e03ff3f8a210851394a31c9c25

--- devstack/ceilosca.sh
+++ devstack/ceilosca.sh
@@ -1,5 +1,11 @@
 #!/bin/bash -xe
 
+echo "going to copy /monasca-ceilometer-source to /home/vagrant ..."
+rsync -a --exclude='tools/vagrant/.vagrant' /monasca-ceilometer-source /home/vagrant/
+
+echo "going to move /home/vagrant/monasca-ceilometer-source /home/vagrant/monasca-ceilometer..."
+mv /home/vagrant/monasca-ceilometer-source /home/vagrant/monasca-ceilometer
+
 #Change to Home directory
 cd $HOME
 
@@ -85,10 +91,33 @@ MONASCA_PERSISTER_IMPLEMENTATION_LANG=${MONASCA_PERSISTER_IMPLEMENTATION_LANG:-p
 # This line will enable all of Monasca.
 enable_plugin monasca-api https://git.openstack.org/openstack/monasca-api stable/newton
 enable_plugin ceilometer https://git.openstack.org/openstack/ceilometer stable/newton
-enable_plugin ceilosca https://github.com/openstack/monasca-ceilometer master
 # Optional
 #enable_plugin monasca-transform https://git.openstack.org/openstack/monasca-transform master
 ' > local.conf
+
+
+pushd /home/vagrant/monasca-ceilometer
+
+echo "creating a local commit..."
+git config --global user.email "local.devstack.committer@hpe.com"
+git config --global user.name "Local devstack committer"
+git add --all
+git commit -m "Local commit"
+echo "creating a local commit done."
+
+CURRENT_BRANCH=`git status | grep 'On branch' | sed 's/On branch //'`
+if [ ${CURRENT_BRANCH} != 'master' ]
+then
+    echo "Maintaining current branch ${CURRENT_BRANCH}"
+    # set the branch to what we're using in local.conf
+    if [[ -z `grep ${CURRENT_BRANCH} /home/vagrant/devstack/local.conf` ]]; then
+        sed -i "s/enable_plugin ceilosca \/home\/vagrant\/monasca-ceilometer//g" /home/vagrant/devstack/local.conf
+        sed -i "s/# END DEVSTACK LOCAL.CONF CONTENTS//g" /home/vagrant/devstack/local.conf
+        printf "enable_plugin ceilosca /home/vagrant/monasca-ceilometer ${CURRENT_BRANCH}\n" >> /home/vagrant/devstack/local.conf
+        printf "# END DEVSTACK LOCAL.CONF CONTENTS" >> /home/vagrant/devstack/local.conf
+    fi
+fi
+popd
 
 #Run the stack.sh
 ./unstack.sh | true
